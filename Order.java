56152edf5c0477cfa57308e25448400e10540a14
Hari Prakash <summithari@gmail.com>
2024-03-13 10:20:46 -0700

    Create Order.java

---
+++ Order.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+
+public class Order{
+  private int orderID;
+  private Customer customer;
+  private String date;
+  private LinkedList<Product> orderContents; //fill in Product based on your own product class
+  private int shippingSpeed; //or use enums
+  private int priority;
+
+  //getters, setters, constructors go here
+
+}
