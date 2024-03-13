d80065937342e5a8b8f3b34b41232d27d01feb7c
Hari Prakash <summithari@gmail.com>
2024-03-13 10:27:38 -0700

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,24 @@
+import java.util.LinkedList;
+
+public class Employee extends User {
+  public Employee(String firstName, String lastName, String login, String password, boolean isManager) {
+    setFirstName(firstName);
+    setLastName(lastName);
+    setLogin(login);
+    setPassword(password);
+
+    this.isManager = isManager; // ??	
+  }
+
+  public boolean isManager() {
+	  return isManager;
+	}
+
+  public void setManager(boolean isManager) {
+	  this.isManager = isManager;
+	}
+
+  public void setFirstName(String firstName) {
+    
+  }
+}
