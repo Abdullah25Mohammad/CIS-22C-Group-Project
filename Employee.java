1be3c35a8b1148c44e64476c7320614916ea7c9e
Abdullah Mohammad <abdullah25mohammad@gmail.com>
2024-03-19 18:50:48 -0700

    Updated Format and General Clean Up

--- Employee.java
+++ Employee.java
@@ -1,24 +1,22 @@
-import java.util.LinkedList;
-
 public class Employee extends User {
-  public Employee(String firstName, String lastName, String login, String password, boolean isManager) {
-    setFirstName(firstName);
-    setLastName(lastName);
-    setLogin(login);
-    setPassword(password);
+    public Employee(String firstName, String lastName, String login, String password, boolean isManager) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setLogin(login);
+        setPassword(password);
 
-    this.isManager = isManager; // ??	
-  }
+        this.isManager = isManager; // ??
+    }
 
-  public boolean isManager() {
-	  return isManager;
-	}
+    public boolean isManager() {
+        return isManager;
+    }
 
-  public void setManager(boolean isManager) {
-	  this.isManager = isManager;
-	}
+    public void setManager(boolean isManager) {
+        this.isManager = isManager;
+    }
 
-  public void setFirstName(String firstName) {
-    
-  }
+    public void setFirstName(String firstName) {
+
+    }
 }
