9f5de772aeeb232ab75c488671f0fba72ca24514
Abdullah Mohammad <abdullah25mohammad@gmail.com>
2024-03-23 13:38:40 -0700

    Added createLoginTable

--- Main.java
+++ Main.java
@@ -4,8 +4,71 @@
  * @author Abdullah Mohammad
  * @author [more names here]
  */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
+    private static HashTable<Customer> customerTable = new HashTable<Customer>(10);
+    private static HashTable<Employee> employeeTable = new HashTable<Employee>(10);
     public static void main(String[] args) {
-        
+        createLoginTable();
+
+        System.out.println("Customer Table:");
+        System.err.println(customerTable.toString());
+        System.out.println("Employee Table:");
+        System.err.println(employeeTable.toString());
     }
+
+    public static void createLoginTable() {
+        try {
+            File file = new File("users.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String firstName = scanner.next();
+                String lastName = scanner.next();
+                scanner.nextLine(); // clear the buffer
+
+                String login = scanner.nextLine();
+                String password = scanner.nextLine();
+                String role = scanner.nextLine();
+
+                if (role.equals("customer")) {
+                    Customer customer = new Customer(
+                        firstName,
+                        lastName,
+                        login,
+                        password
+                    );
+                    customerTable.add(customer);
+                } else if (role.equals("employee")) {
+                    Employee employee = new Employee(
+                        firstName,
+                        lastName,
+                        login,
+                        password,
+                        false
+                    );
+                    employeeTable.add(employee);
+                } else if (role.equals("manager")) {
+                    Employee employee = new Employee(
+                        firstName,
+                        lastName,
+                        login,
+                        password,
+                        true
+                    );
+                    employeeTable.add(employee);
+                } else {
+                    System.out.println("Invalid role: " + role);
+                }
+                
+            }
+            scanner.close();
+        } catch (FileNotFoundException error) {
+            System.out.println("An error occurred.");
+            error.printStackTrace();
+        }
+    }
+    
 }
