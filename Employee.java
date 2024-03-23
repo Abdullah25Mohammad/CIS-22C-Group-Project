2e140f7289d844f2ef92b262c0cf8352dbf6c48c
Abdullah Mohammad <abdullah25mohammad@gmail.com>
2024-03-23 13:10:29 -0700

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,7 +1,23 @@
+/**
+ * Employee.java
+ * Represents an employee who is a user of the system.
+ * 
+ * @author Abdullah Mohammad
+ */
 public class Employee extends User {
     private boolean isManager;
 
 
+    /**
+     * Constructor for the Employee class
+     * 
+     * @param firstName
+     * @param lastName
+     * @param login
+     * @param password
+     * @param isManager
+     * @postcondition a new Employee object is created with the given values
+     */
     public Employee(String firstName, String lastName, String login, String password, boolean isManager) {
         setFirstName(firstName);
         setLastName(lastName);
@@ -18,4 +34,6 @@ public class Employee extends User {
     public void setManager(boolean isManager) {
         this.isManager = isManager;
     }
+
+
 }
