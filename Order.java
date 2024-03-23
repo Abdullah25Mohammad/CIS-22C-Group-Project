df65853c322c6aeb7d360dc916ff41faf8d6aa18
Abdullah Mohammad <abdullah25mohammad@gmail.com>
2024-03-23 13:10:33 -0700

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,13 +1,155 @@
+/**
+ * Order.java
+ * Order class that represents an order in the system.
+ * 
+ * @author Abdullah Mohammad
+ */
 public class Order {
     private int orderID;
     private Customer customer;
-    private String date;
+    private String datePlaced; // date format: "MM/DD/YYYY"
     private LinkedList<Game> orderContents;
-    private int shippingSpeed; // or use enums
+    private int shippingSpeed;
     private int priority;
 
 
+    /**
+     * Constructor for the Order class
+     * 
+     * @param orderID
+     * @param customer
+     * @param datePlaced
+     * @param orderContents
+     * @param shippingSpeed
+     * @param priority
+     * @postcondition a new Order object is created with the given values
+     */
+    public Order(int orderID, Customer customer, String datePlaced, LinkedList<Game> orderContents, int shippingSpeed, int priority) {
+        this.orderID = orderID;
+        this.customer = customer;
+        this.datePlaced = datePlaced;
+        this.orderContents = orderContents;
+        this.shippingSpeed = shippingSpeed;
+        this.priority = priority;
+    }
 
-    // getters, setters, constructors go here
+    /**** ACCESSORS ****/
+
+    /**
+     * Returns the order ID of the order.
+     *
+     * @return the order ID of the order
+     */
+    public int getOrderID() {
+        return orderID;
+    }
+
+    /**
+     * Returns the customer of the order.
+     *
+     * @return the customer of the order
+     */
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    /**
+     * Returns the datePlaced of the order.
+     *
+     * @return the datePlaced of the order
+     */
+    public String getDate() {
+        return datePlaced;
+    }
+
+    /**
+     * Returns the order contents.
+     *
+     * @return the order contents
+     */
+    public LinkedList<Game> getOrderContents() {
+        return orderContents;
+    }
+
+    /**
+     * Returns the shipping speed of the order.
+     *
+     * @return the shipping speed of the order
+     */
+    public int getShippingSpeed() {
+        return shippingSpeed;
+    }
+
+    /**
+     * Returns the priority of the order.
+     *
+     * @return the priority of the order
+     */
+    public int getPriority() {
+        return priority;
+    }
+
+
+    /**** MUTATORS ****/
+
+    /**
+     * Sets the order ID of the order.
+     *
+     * @param orderID the order ID of the order
+     * @postcondition the order ID of the order is set
+     */
+    public void setOrderID(int orderID) {
+        this.orderID = orderID;
+    }
+
+    /**
+     * Sets the customer of the order.
+     *
+     * @param customer the customer of the order
+     * @postcondition the customer of the order is set
+     */
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    /**
+     * Sets the datePlaced of the order.
+     *
+     * @param datePlaced the datePlaced of the order
+     * @postcondition the datePlaced of the order is set
+     */
+    public void setDate(String datePlaced) {
+        this.datePlaced = datePlaced;
+    }
+
+    /**
+     * Sets the order contents.
+     *
+     * @param orderContents the order contents
+     * @postcondition the order contents are set
+     */
+    public void setOrderContents(LinkedList<Game> orderContents) {
+        this.orderContents = orderContents;
+    }
+
+    /**
+     * Sets the shipping speed of the order.
+     *
+     * @param shippingSpeed the shipping speed of the order
+     * @postcondition the shipping speed of the order is set
+     */
+    public void setShippingSpeed(int shippingSpeed) {
+        this.shippingSpeed = shippingSpeed;
+    }
+
+    /**
+     * Sets the priority of the order.
+     *
+     * @param priority the priority of the order
+     * @postcondition the priority of the order is set
+     */
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
 
 }
