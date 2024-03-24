b00dbd0a9920aa4d9e21bda74d9a719b82505989
ChahidBagdouri <162845507+ChahidBagdouri@users.noreply.github.com>
2024-03-24 15:19:49 -0700

    Added login() to main
    Added a login() method to main.
    Replaced all "login" variables with "username".

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@
  * Main class
  * 
  * @author Abdullah Mohammad
+ * @author Chahid Bagdouri
  * @author [more names here]
  */
 import java.io.File;
@@ -12,15 +13,19 @@ public class Main {
     private static HashTable<Customer> customerTable = new HashTable<Customer>(10);
     private static HashTable<Employee> employeeTable = new HashTable<Employee>(10);
     public static void main(String[] args) {
-        createLoginTable();
+        createLoginTables();
 
-        System.out.println("Customer Table:");
-        System.err.println(customerTable.toString());
-        System.out.println("Employee Table:");
-        System.err.println(employeeTable.toString());
+        // System.out.println("Customer Table:");
+        // System.err.println(customerTable.toString());
+        // System.out.println("Employee Table:");
+        // System.err.println(employeeTable.toString());
+
+        System.out.println("Welcome to the Video Games Store!");
+        System.out.println("Please login to continue.");
+        login();
     }
 
-    public static void createLoginTable() {
+    public static void createLoginTables() {
         try {
             File file = new File("users.txt");
             Scanner scanner = new Scanner(file);
@@ -29,7 +34,7 @@ public class Main {
                 String lastName = scanner.next();
                 scanner.nextLine(); // clear the buffer
 
-                String login = scanner.nextLine();
+                String username = scanner.nextLine();
                 String password = scanner.nextLine();
                 String role = scanner.nextLine();
 
@@ -37,7 +42,7 @@ public class Main {
                     Customer customer = new Customer(
                         firstName,
                         lastName,
-                        login,
+                        username,
                         password
                     );
                     customerTable.add(customer);
@@ -45,7 +50,7 @@ public class Main {
                     Employee employee = new Employee(
                         firstName,
                         lastName,
-                        login,
+                        username,
                         password,
                         false
                     );
@@ -54,7 +59,7 @@ public class Main {
                     Employee employee = new Employee(
                         firstName,
                         lastName,
-                        login,
+                        username,
                         password,
                         true
                     );
@@ -70,5 +75,33 @@ public class Main {
             error.printStackTrace();
         }
     }
-    
+   
+
+    public static void login() {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Please enter your username:");
+        String username = myScanner.nextLine();
+        System.out.println("Please enter your password:");
+        String password = myScanner.nextLine();
+
+        Customer tempCustomer = new Customer("", "", username, password);
+        boolean isCustomer = (customerTable.find(tempCustomer) != -1);
+        if(isCustomer) {
+            tempCustomer = customerTable.get(tempCustomer);
+            System.out.println("Successfully logged in as " + tempCustomer.getFirstName() + " " + tempCustomer.getLastName());
+        }
+        else {
+            Employee tempEmployee = new Employee("", "", username, password, false);
+            boolean isEmployee = (employeeTable.find(tempEmployee) != -1);
+            if(isEmployee) {
+                tempEmployee = employeeTable.get(tempEmployee);
+                System.out.println("Successfully logged in as " + tempEmployee.getFirstName() + " " + tempEmployee.getLastName());
+            }
+            else {
+                System.out.println("Login failed.");
+            }
+        }
+
+        myScanner.close();
+    }
 }
