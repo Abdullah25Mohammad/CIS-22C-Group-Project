86e62e9882f8ec587ff13fd9bebceecb4a2cbb69
Abdullah Mohammad <abdullah25mohammad@gmail.com>
2024-03-24 14:38:03 -0700

    Update Date.java

--- Date.java
+++ Date.java
@@ -1,13 +1,25 @@
+import java.util.Comparator;
+
 public class Date {
     final int month;
     final int day;
     final int year;
+    
+    // Comparator<Date> dateComparator = (d1, d2) ->
+    //     Integer.compare(d1.year, d2.year) != 0 ? Integer.compare(d1.year, d2.year) :
+    //     Integer.compare(d1.month, d2.month) != 0 ? Integer.compare(d1.month, d2.month) :
+    //     Integer.compare(d1.day, d2.day);
 
     public Date(int month, int day, int year) {
         this.month = month;
         this.day = day;
         this.year = year;
     }
+
+    // public Comparator<Date> getDateComparator()
+    // {
+    //     return dateComparator;
+    // }
 
     @Override
     public String toString() {
@@ -16,24 +28,25 @@ public class Date {
 
     @Override
     public boolean equals(Object obj) {
-        if (obj instanceof Date) {
-            Date other = (Date) obj;
-            return month == other.month && day == other.day && year == other.year;
-        } else {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof Date)) {
             return false;
         }
+        Date d = (Date) obj;
+        return d.month == month && d.day == day && d.year == year;
     }
 
-    public int compareTo(Date other) {
-        if (year != other.year) {
-            return Integer.compare(year, other.year);
-        } else if (month != other.month) {
-            return Integer.compare(month, other.month);
+    public int compareTo(Date d) {
+        if (year != d.year) {
+            return Integer.compare(year, d.year);
+        } else if (month != d.month) {
+            return Integer.compare(month, d.month);
         } else {
-            return Integer.compare(day, other.day);
+            return Integer.compare(day, d.day);
         }
     }
-
 } // end of Date class
 
 // class dateComparator implements Comparator<Date> {
