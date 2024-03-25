c72e964396fbf8a78dde5b3d3f49e722a36aee3d
ChahidBagdouri <162845507+ChahidBagdouri@users.noreply.github.com>
2024-03-25 06:51:35 -0700

    Expand login() into 4 separate methods

--- Main.java
+++ Main.java
@@ -10,6 +10,9 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
+    private static Scanner myScanner = new Scanner(System.in);
+
+    // HashTables
     private static HashTable<Customer> customerTable = new HashTable<Customer>(10);
     private static HashTable<Employee> employeeTable = new HashTable<Employee>(10);
 
@@ -31,8 +34,24 @@ public class Main {
         // System.err.println(employeeTable.toString());
 
         System.out.println("Welcome to the Video Games Store!");
-        System.out.println("Please login to continue.");
-        login();
+        System.out.println("Would you like to login as a Customer or Employee?");
+
+        int choice = -1;
+        while(choice != 1 && choice != 2) {
+            System.out.println("Type '1' for Customer and '2' for Employee.");
+            choice = Integer.parseInt(myScanner.nextLine());
+            if(choice == 1) {
+                loginAsCustomer();
+                break;
+            }
+            if(choice == 2) {
+                loginAsEmployee();
+                break;
+            }        
+            else {
+                System.out.println("Please try again.");
+            }
+        }
     }
 
     public static void createLoginTables() {
@@ -46,7 +65,8 @@ public class Main {
 
                 String username = scanner.nextLine();
                 String password = scanner.nextLine();
-                String role = scanner.nextLine();
+                String role = scanner.next();
+                scanner.nextLine(); // clear the buffer
 
                 if (role.equals("customer")) {
                     Customer customer = new Customer(
@@ -85,7 +105,7 @@ public class Main {
             error.printStackTrace();
         }
     }
-
+   
     public static void createDatabase() {
         try {
             File file = new File("database.txt");
@@ -136,31 +156,108 @@ public class Main {
         }
     }
 
-    public static void login() {
-        Scanner myScanner = new Scanner(System.in);
+    public static void loginAsCustomer() {
+        System.out.println("Would you like to make a new account or login to an existing one?");
+        System.out.println("Type '1' for new account and '2' for existing account.");
+        int choice = Integer.parseInt(myScanner.nextLine());
+
+        while(choice != 1 && choice != 2) {
+            System.out.println("Please try again.");
+            System.out.println("Type '1' for new account and '2' for existing account.");
+            choice = Integer.parseInt(myScanner.nextLine());
+        }
+
+        Customer tempCustomer;
+        
+        if(choice == 1) {
+            tempCustomer = loginAsNewCustomer();
+        }
+        
+        else if(choice == 2) {
+            tempCustomer = loginAsExistingCustomer();
+        }
+    }
+
+    public static Customer loginAsNewCustomer() {
+        System.out.print("Please enter your first name: ");
+        String firstName = myScanner.next();
+        System.out.print("Please enter your last name: ");
+        String lastName = myScanner.next();
+        System.out.print("Please enter a username: ");
+        String username = myScanner.next();
+        System.out.print("Please enter a password: ");
+        String password = myScanner.next();
+
+        Customer tempCustomer = new Customer(firstName, lastName, username, password);
+
+        if((customerTable.find(tempCustomer) != -1)) {
+            System.out.println("The user with the information that you provided already exists.");
+            tempCustomer = customerTable.get(tempCustomer);
+            System.out.println("Successfully logged in as existing user " + tempCustomer.getFirstName() + " " + tempCustomer.getLastName());
+        }
+
+        else {  
+            customerTable.add(tempCustomer);
+
+            System.out.println("Successfully created account for and logged in as " + tempCustomer.getFirstName() + " " + tempCustomer.getLastName());
+        }
+
+        return tempCustomer;
+    }
+    
+    public static Customer loginAsExistingCustomer() {
+        System.out.print("Please enter your username: ");
+        String username = myScanner.next();
+        System.out.print("Please enter your password: ");
+        String password = myScanner.next();
+
+        Customer tempCustomer = new Customer("", "", username, password);
+
+        if((customerTable.find(tempCustomer) != -1)) {
+            tempCustomer = customerTable.get(tempCustomer);
+            System.out.println("Successfully logged in as " + tempCustomer.getFirstName() + " " + tempCustomer.getLastName());
+        }
+
+        else {
+            System.out.println("Username and password do not match.");
+            System.out.println("Would you like to try again or make a new account?");
+            System.out.println("Type '1' to try again or '2' for new account.");
+            myScanner.nextLine(); // clear the buffer
+            int choice = Integer.parseInt(myScanner.nextLine());
+
+            while(choice != 1 && choice != 2) {
+                System.out.println("Please try again.");
+                System.out.println("Type '1' to try again or '2' for new account.");
+                myScanner.nextLine(); // clear the buffer
+                choice = Integer.parseInt(myScanner.nextLine());
+            }
+
+            if(choice == 1) {
+                tempCustomer = loginAsExistingCustomer();
+            }
+            else if (choice == 2) {
+                tempCustomer = loginAsNewCustomer();
+            }
+        }
+
+        return tempCustomer;
+    }
+    
+    public static void loginAsEmployee() {
         System.out.println("Please enter your username:");
         String username = myScanner.nextLine();
         System.out.println("Please enter your password:");
         String password = myScanner.nextLine();
 
-        Customer tempCustomer = new Customer("", "", username, password);
-        boolean isCustomer = (customerTable.find(tempCustomer) != -1);
-        if(isCustomer) {
-            tempCustomer = customerTable.get(tempCustomer);
-            System.out.println("Successfully logged in as " + tempCustomer.getFirstName() + " " + tempCustomer.getLastName());
+
+        Employee tempEmployee = new Employee("", "", username, password, false);
+        boolean isEmployee = (employeeTable.find(tempEmployee) != -1);
+        if(isEmployee) {
+            tempEmployee = employeeTable.get(tempEmployee);
+            System.out.println("Successfully logged in as " + tempEmployee.getFirstName() + " " + tempEmployee.getLastName());
         }
         else {
-            Employee tempEmployee = new Employee("", "", username, password, false);
-            boolean isEmployee = (employeeTable.find(tempEmployee) != -1);
-            if(isEmployee) {
-                tempEmployee = employeeTable.get(tempEmployee);
-                System.out.println("Successfully logged in as " + tempEmployee.getFirstName() + " " + tempEmployee.getLastName());
-            }
-            else {
-                System.out.println("Login failed.");
-            }
+            System.out.println("Login failed.");
         }
-
-        myScanner.close();
     }
 }
